47771d495b1cec880050e22e07eeefcf2cfd356f
Rex Tsai <chihchun@kalug.linux.org.tw>
2009-10-28 22:36:26 +0800

    refined the codebsae.

--- stroke.js
+++ stroke.js
@@ -9,10 +9,12 @@
 ;(function($) { // Hide scope, no $ conflict
 
 function StrokeHandler (root) {
-   this._root = root; 
    this._xml;
-   this.svg;
 
+   // create a new svg
+   $(root).svg();
+   // get the svg 
+   this.svg = $(root).svg('get');
 
    this.strokeN = 0;
    this.trackN = 0;
@@ -26,12 +28,8 @@ $.extend(StrokeHandler.prototype, {
 
         loadWord: function () {
              var handler = this;
-             // create a new svg
-             $('#svgbasics').svg();
-             // get the svg 
-             this.svg = $('#svgbasics').svg('get');
-	     this.strokeNum = 0;
-             
+             var strokeNum = 0;
+
              $.ajax({
                 type: "GET",
                 url: "B3B0.xml",
@@ -40,111 +38,74 @@ $.extend(StrokeHandler.prototype, {
 		success: function(xml) {
                     handler._xml = $(xml);
                     $(xml).find('Stroke').each(function() {
-                        handler.drawStroke(this);
+                        handler.drawStroke(this, strokeNum);
+                        strokeNum++;
                     });
+                    // playback
 		}
 	     });
         },
 
-
 	clean: function () {
-		this.svg = $('#svgbasics').svg('get');
-		this.svg.clear();
-		// this.svg.
+            this.svg.clear();
 	},
 
-	drawStrokeMask: function (xmlTag) {
+	playback: function () {
+            var path = this.path;
+            var svg = this.svg;
 
-	},
+            if(this._xml == null)
+                return;
 
-	playback: function () 
-	{
-		var path = this.path;
-                var svg = this.svg;
+            var wordMask = "url(#WordMask" + this.strokeN + ")";
 
-		// the xml is not ready
-		if(this._xml == null)
-			return;
-		var wordMask = "url(#WordMask" + this.strokeN + ")";
+            var group = this.svg.group();
+            group.setAttribute("transform", "scale(0.1, 0.1)");
+            group.setAttribute("mask", wordMask);
 
-                this.group = this.svg.group();
-                this.group.setAttribute("transform", "scale(0.1, 0.1)");
-                this.group.setAttribute("mask", wordMask);
+            var stroke = this._xml.find('Stroke');
+            if(stroke.length < this.strokeN) {
+                return;
+            }
 
-		xmlTag = this._xml.find('Stroke').get(this.strokeN);
+            var track = $(stroke.get(this.strokeN)).find('Track > MoveTo');
+            if(track.length < this.trackN) {
+                this.strokeN++;
+                this.trackN = 0;
+                return;
+            }
 
-                var stroke = this._xml.find('Stroke');
-                if(stroke.length < this.strokeN)
-                {
-                    return;
-                }
+            track = track.get(this.trackN);
 
+            if(this.trackN == 0) {
+                path = svg.createPath();
+                path = path.move($(track).attr('x'), $(track).attr('y'), false);
+            }
+            path = path.line($(track).attr('x'), $(track).attr('y'), false);
+            svg.circle(group, $(track).attr('x'), $(track).attr('y'), 100, 
+                    {fill: 'black', stroke: 'blue', strokeWidth: 0});
+            svg.path(group, path, 
+                    {fill: 'red', stroke: 'black', strokeWidth: 100});
 
-                var moveto = $(xmlTag).find('Track > MoveTo');
-                if(moveto.length < this.trackN)
-                {
-                    this.strokeN++
-                    this.trackN = 0;
-                    return;
-                }
-
-		xmlTag = $(xmlTag).find('Track > MoveTo').get(this.trackN);
-
-		if(this.trackN == 0) {
-	            path = svg.createPath();
-  	            path = path.move($(xmlTag).attr('x'), $(xmlTag).attr('y'), false);
-		}
-		path = path.line($(xmlTag).attr('x'), $(xmlTag).attr('y'), false);
-                svg.circle(this.group, $(xmlTag).attr('x'), $(xmlTag).attr('y'), 100, 
-			{fill: 'black', stroke: 'blue', strokeWidth: 0});
-                svg.path(this.group, path, 
-			{fill: 'red', stroke: 'black', strokeWidth: 100});
-
-		this.trackN++;
-                this.path = path;
+            this.trackN++;
+            this.path = path;
 	},
 	
 
-        drawStroke: function (xmlTag) {
-                var handler = this;
-                var svg = this.svg;
-                var path = svg.createPath(); 
-		var strokeNum = this.strokeNum;
-		var wordMask = "url(#WordMask" + this.strokeNum + ")";
-		mask = this.svg.mask(null, 'WordMask'+this.strokeNum, 0, 0, 2000, 2000, {maskUnits: 'userSpaceOnUse'});
-                this.group = svg.group(mask);
+        drawStroke: function (xmlTag, strokeNum) {
+            var handler = this;
+            var svg = this.svg;
 
-                // outline
-                $(xmlTag).find('Outline').each ( function () {
-                        path = svg.createPath();
-                        $(this).children().each( function () {
-                            handler._drawOutline(this, path);
-                        });
-                        svg.path(handler.group, path, {fill: 'white', stroke: '#D90000', strokeWidth: 1});
-                });
-
-                // tracks
-                // this.group = this.svg.group();
-                // this.group.setAttribute("transform", "scale(0.1, 0.1)");
-                // this.group.setAttribute("id", this.strokeNum);
-
-		// initalize the path.
-                var loc =  $(xmlTag).find('Track > MoveTo').get(0);
-                path = svg.createPath();
-                path = path.move($(loc).attr('x'), $(loc).attr('y'), false);
-
-                $(xmlTag).find('Track').children().each ( function () {
-                        var xmlTag = this;
-                        if(this.tagName == 'MoveTo') {
-                            // path = path.line($(xmlTag).attr('x'), $(xmlTag).attr('y'), false);
-                            // svg.circle(handler.group, $(xmlTag).attr('x'), $(xmlTag).attr('y'), 100, 
-				// {fill: 'black', stroke: 'blue', strokeWidth: 0, mask: wordMask});
-                        }
-                });
-                // svg.path(handler.group, path, 
-		//	{fill: 'red', stroke: 'black', strokeWidth: 100, mask: wordMask});
-
-		this.strokeNum++;
+            var mask = this.svg.mask(null, 'WordMask'+strokeNum, 0, 0, 2000, 2000, {maskUnits: 'userSpaceOnUse'});
+            this.group = svg.group(mask);
+            // outline
+            $(xmlTag).find('Outline').each ( function () {
+                var path = svg.createPath();
+                $(this).children().each( function () {
+                    handler._drawOutline(this, path);
+                    });
+                svg.path(handler.group, path, {fill: 'white', stroke: '#D90000', strokeWidth: 1});
+            });
         },
 
         _drawOutline: function(xmlTag, path) {
@@ -153,34 +114,16 @@ $.extend(StrokeHandler.prototype, {
             }
 
             if(xmlTag.tagName == 'QuadTo') {
-                path = path.curveQ(
-                    $(xmlTag).attr('x1'), 
-                    $(xmlTag).attr('y1'),
-                    $(xmlTag).attr('x2'),
-                    $(xmlTag).attr('y2'), 
-                    false);
+                path = path.curveQ( $(xmlTag).attr('x1'), $(xmlTag).attr('y1'), $(xmlTag).attr('x2'), $(xmlTag).attr('y2'), false);
             }
 
             if(xmlTag.tagName == 'LineTo') {
-                path = path.line(
-                    $(xmlTag).attr('x'), 
-                    $(xmlTag).attr('y')
-                    );
+                path = path.line( $(xmlTag).attr('x'), $(xmlTag).attr('y'));
             }
 
             if(xmlTag.tagName == 'CubicTo') {
-                path = path.curveQ(
-                        $(xmlTag).attr('x1'), 
-                        $(xmlTag).attr('y1'),
-                        $(xmlTag).attr('x2'),
-                        $(xmlTag).attr('y2'), 
-                        false);
-                path = path.curveQ(
-                        $(xmlTag).attr('x2'), 
-                        $(xmlTag).attr('y2'),
-                        $(xmlTag).attr('x3'),
-                        $(xmlTag).attr('y3'), 
-                        false);
+                path = path.curveQ( $(xmlTag).attr('x1'), $(xmlTag).attr('y1'), $(xmlTag).attr('x2'), $(xmlTag).attr('y2'), false);
+                path = path.curveQ( $(xmlTag).attr('x2'), $(xmlTag).attr('y2'), $(xmlTag).attr('x3'), $(xmlTag).attr('y3'), false);
             }
         },
 
